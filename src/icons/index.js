50ea363a2ed0ee09024504f00edeeaaf4b915341
JoseAvilan <40833283+JoseAvilan@users.noreply.github.com>
2025-04-11 22:05:34 -0400

    refactor: eliminar archivo index.js de íconos y usar importaciones directas

--- src/icons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home.astro";
-export { default as User } from "./User.astro";
-export { default as Projects } from "./Projects.astro";
-export { default as Contact } from "./Contact.astro";
